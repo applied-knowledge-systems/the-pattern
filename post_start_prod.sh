8856800d498322e28ca930d0f769b69610e7ddb9
Alex Mikhalev <alex.mikhalev@gmail.com>
2021-05-10 22:27:56 +0200

    Removed storage from rgcluster

--- post_start_prod.sh
+++ post_start_prod.sh
@@ -1,4 +1,5 @@
 docker-compose -f docker-compose.prod.yml exec rgcluster /cluster/create-cluster call RG.CONFIGSET ExecutionMaxIdleTime 300000
 docker-compose -f docker-compose.prod.yml exec rgcluster /cluster/create-cluster call CONFIG SET proto-max-bulk-len 2048mb
 docker-compose -f docker-compose.prod.yml exec rgcluster /cluster/create-cluster call CONFIG SET list-compress-depth 1
-docker-compose -f docker-compose.prod.yml exec rgcluster /cluster/create-cluster call CONFIG SET cluster-node-timeout 30000
+docker-compose -f docker-compose.prod.yml exec rgcluster /cluster/create-cluster call CONFIG SET cluster-node-timeout 30000 
+#docker-compose -f docker-compose.prod.yml exec rgcluster /cluster/create-cluster call CONFIG SET SAVE ""
