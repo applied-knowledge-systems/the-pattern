2104aa5f143b06e8848f706df407655d0f5435c0
Alex Mikhalev <alex@sci-blog.com>
2021-11-02 18:17:32 +0000

    Redisai latest tag

--- debs.sh
+++ debs.sh
@@ -1,5 +1,5 @@
 git clone --depth 1 --recursive https://github.com/RedisGears/RedisGears.git
 cd RedisGears
-git submodule update --init --recursive
+git submodule update --init --recursive 
 cd .. 
 docker-compose -f docker-compose.debs.yml build
