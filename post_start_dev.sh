a713cc3fe370c1c72d0b1833fd0b19ee99cdec93
Alex Mikhalev <alex@sci-blog.com>
2022-04-15 21:00:57 +0100

    No persistence in cluster
    Signed-off-by: Alex Mikhalev <alex@sci-blog.com>

--- post_start_dev.sh
+++ post_start_dev.sh
@@ -3,4 +3,3 @@ docker-compose -f docker-compose.dev.yml exec rgcluster /cluster/create-cluster 
 docker-compose -f docker-compose.dev.yml exec rgcluster /cluster/create-cluster call CONFIG SET list-compress-depth 1
 docker-compose -f docker-compose.dev.yml exec rgcluster /cluster/create-cluster call CONFIG SET cluster-node-timeout 30000 
 docker-compose -f docker-compose.dev.yml exec rgcluster /cluster/create-cluster call config set appendonly no
-docker-compose -f docker-compose.dev.yml exec rgcluster /cluster/create-cluster call config set save ""
