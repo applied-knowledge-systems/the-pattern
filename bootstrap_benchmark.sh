c75b6489cbb3e24c1f0cd58a0a64fdf55427775e
AlexMikhalev <alex@sci-blog.com>
2022-04-15 17:14:36 +0100

    Update bootstrap_benchmark.sh
    Pinned gears-cli==1.1.3

--- bootstrap_benchmark.sh
+++ bootstrap_benchmark.sh
@@ -5,7 +5,7 @@ sudo apt install python3.9-venv
 python3 -m venv ./venv_the_pattern
 source ./venv_the_pattern/bin/activate
 cd the-pattern-platform && pip3 install -r requirements.txt
-pip3 install gears-cli 
+pip3 install gears-cli==1.1.3 
 ./cluster_pipeline.sh
 cd ../the-pattern-api/qasearch/ 
 sh start_benchmark.sh 
